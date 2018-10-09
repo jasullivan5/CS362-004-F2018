8d71271c084251044d9ce7f685ea1efd523d2ee3
Joseph Sullivan <sullijos@oregonstate.edu>
2018-10-08 22:51:31 -0700

    added smithy function

--- projects/sullijos/dominion/dominion.c
+++ projects/sullijos/dominion/dominion.c
@@ -829,15 +829,8 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
       return 0;
 		
     case smithy:
-      //+3 Cards
-      for (i = 0; i < 3; i++)
-	{
-	  drawCard(currentPlayer, state);
-	}
-			
-      //discard card from hand
-      discardCard(handPos, currentPlayer, state, 0);
-      return 0;
+	//+3 Cards
+	return playSmithy(currentPlayer, state, handPos);
 		
     case village:
       //+1 Card
@@ -1221,6 +1214,20 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
     }
 	
   return -1;
+}
+
+// New smithy function for assignment 2
+int playSmithy(int currentPlayer, struct gameState *state, int handPos)
+{
+	// draw 3 cards
+	int i;
+	for (i = 0; i < 3; i++)
+	{
+		drawCard(currentPlayer, state);
+	}		
+      	//discard card from hand
+      	discardCard(handPos, currentPlayer, state, 0);
+      	return 0;
 }
 
 int discardCard(int handPos, int currentPlayer, struct gameState *state, int trashFlag)
--- projects/sullijos/dominion/dominion.h
+++ projects/sullijos/dominion/dominion.h
@@ -128,4 +128,7 @@ int getWinners(int players[MAX_PLAYERS], struct gameState *state);
 /* Set array position of each player who won (remember ties!) to
    1, others to 0 */
 
+int playSmithy(int currentPlayer, struct gameState *state, int handPos);
+/* Smithy function for assignment 2 */
+
 #endif
