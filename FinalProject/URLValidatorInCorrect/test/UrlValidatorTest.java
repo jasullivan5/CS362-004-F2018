3563c4966d6ae01e7db7771db2f7f27d25739c69
jasullivan5 <31702825+jasullivan5@users.noreply.github.com>
2018-11-30 10:51:20 -0800

    refresh of finalproject folders

--- FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -1,21 +1,31 @@
+
+
 import junit.framework.TestCase;
+
 //You can use this as a skeleton for your 3 different test approach
 //It is an optional to use this file, you can generate your own test file(s) to test the target function!
 // Again, it is up to you to use this file or not!
+
+
+
+
+
 public class UrlValidatorTest extends TestCase {
 
-	public UrlValidatorTest(String testName) {
+
+   public UrlValidatorTest(String testName) {
       super(testName);
    }
 
+   
+   
    public void testManualTest()
    {
 //You can use this function to implement your manual testing	   
-	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
-	  assertTrue(urlVal.isValid("http://www.google.com")); // valid scheme, valid authority
-	  assertTrue(urlVal.isValid("https://www.google.com")); // invalid scheme, valid authority
+	   
    }
-      
+   
+   
    public void testYourFirstPartition()
    {
 	 //You can use this function to implement your First Partition testing	   
@@ -34,9 +44,6 @@ public class UrlValidatorTest extends TestCase {
 
    }
    
-   public static void main(String[] argv) {
-	   UrlValidatorTest fct = new UrlValidatorTest("url test");
-	   fct.testManualTest();
-   }
+
 
 }
